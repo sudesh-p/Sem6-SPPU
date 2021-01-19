6480aab7d7176688027b9271d3534239fe1c37d6
Sudesh Pawar <sudesh2911@gmail.com>
2021-01-19 09:26:45 +0530

    Create KPDT.java

---
+++ SPOSL/Group A/Assignment A4/src/com/pass_two_macro/KPDT.java
@@ -0,0 +1,40 @@
+package com.pass_two_macro;
+
+import java.util.HashMap;
+
+public class KPDT {
+	HashMap<Integer,HashMap<String,String>>kpdt;
+	
+	KPDT(){
+		kpdt=new HashMap<>();
+	}
+	
+	public void insert(String parameter,String value,int kpdtp){
+		if(!kpdt.containsKey(kpdtp)){
+			kpdt.put(kpdtp,new HashMap<String,String>());
+		}
+		kpdt.get(kpdtp).put(parameter, value);
+	}
+	
+	public String get(String parameter,int pointer){
+		if(kpdt.containsKey(pointer)){
+			if(kpdt.get(pointer).containsKey(parameter)){
+				return kpdt.get(pointer).get(parameter);
+			}
+			return "Invalid Parameter";
+		}
+		return "Invalid KPD Parameter";
+	}
+	
+	public void print(){
+		for (Integer count: kpdt.keySet()){
+            HashMap<String,String>temp = kpdt.get(count);  
+            System.out.print(count+" ");
+        	for (String param: temp.keySet())
+          {
+                    String value = temp.get(param);  
+                    System.out.print(param + " " + value+"\n");  
+        	} 
+        } 
+	} 
+}
